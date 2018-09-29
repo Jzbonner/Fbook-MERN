f944ccf9d5f9b987bb69123c839f171afa3c6308
Jzbonner <jarrett.bonner@gmail.com>
2018-09-29 19:07:31 -0400

    changes to Fbook MERN structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './css/CommentBox.css';
+import CommentBox from './CommentBox';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(<CommentBox />, 
+    document.getElementById('root'));
+
